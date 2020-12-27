04a338da97394c1e701e1bb4abc317f1dfe8d47e
Anatoly <anatoly.safrygin@gmail.com>
2020-12-28 01:14:59 +0500

    urls added

---
+++ blogengine/blog/urls.py
@@ -0,0 +1,8 @@
+from django import urls
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', post_list)
+]
---
+++ blogengine/blog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse
+
+
+def post_list(requset):
+    return HttpResponse('<h1>Hello, world!</h1>')
