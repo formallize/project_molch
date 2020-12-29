34f4cd88a33d26cdcf47af6b46045b0933bf3049
Anatoly <anatoly.safrygin@gmail.com>
2020-12-29 22:55:38 +0500

    class_views and get_object_or_404 added

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
+
 from .models import *
 
 
@@ -6,14 +9,26 @@ def post_list(requset):
     posts = Post.objects.all()
     return render(requset, 'blog/index.html', context={'posts':posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post':post})
+# def post_detail(request, slug):
+#   post = Post.objects.get(slug__iexact=slug)
+#    return render(request, 'blog/post_detail.html', context={'post':post})
+
+class PostDetail(View):
+    def get(self, request, slug):
+        #post = Post.objects.get(slug__iexact=slug)
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post':post})
 
 def tag_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tag_list.html', context={'tags':tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag':tag})
+class TagDetail(View):
+    def get(self, request, slug):
+        #tags = Tag.objects.all()
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag':tag})
+
+#def tag_detail(request, slug):
+#    tag = Tag.objects.get(slug__iexact=slug)
+#    return render(request, 'blog/tag_detail.html', context={'tag':tag})
